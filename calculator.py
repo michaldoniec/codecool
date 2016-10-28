76a6b0b1f507467b18436c32e2d4a1cc45601ec2
michaldoniec <r.pr.michal.doniec@gmail.com>
2016-10-28 19:38:11 +0200

    Add files via upload
    Calculator. Simply program that based on the user's input (integers) will perform basic algorithmic operations on the given numbers. User can terminate program by inserting letter.

---
+++ calculator.py
@@ -0,0 +1,78 @@
+"""Calculator. After user provides two integer numbers and an operator
+program will perform simple mathematic calculations. If user instead
+of number provides letter (from english alphabet) then program will exit.
+"""
+
+
+__version__ = 0.1
+__author__ = "Michal Doniec"
+
+
+import sys
+
+
+alphabet = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",\
+            "r","s","t","u","w","x","y","z")
+
+operations = ("+","-","*","/")
+
+
+def isinteger(s):
+    """Determines whether user's input is an integer."""
+
+    try:
+        int(s)
+    except ValueError:
+        return False
+
+
+a = input("Enter a number (integer) or a letter to exit: ")
+
+while isinteger(a) == False and a.lower() not in alphabet:
+    print("Invalid sign.")
+    a = input("Enter a number (integer) or a letter to exit: ")
+
+while a.lower() not in alphabet:
+    b = input("Enter an operator (+, -, * or /): ")
+    while b not in operations:
+        print("Invalid sign.")
+        b = input("Enter an operation (+, -, * or /): ")
+
+    else:
+        c = input("Enter another number (integer): ")
+        while isinteger(c) == False:
+            print("Invalid sign.")
+            c = input("Enter another number (integer): ")
+
+        else:
+            if b == "+":
+                total = int(a) + int(c)
+                print("Result: " + str(total))
+                a = input("Enter a number (integer) or a letter to exit: ")
+                while isinteger(a) == False and a.lower() not in alphabet:
+                    print("Invalid sign.")
+                    a = input("Enter a number (integer) or a letter to exit: ")
+            elif b == "-":
+                total = int(a) - int(c)
+                print("Result: " + str(total))
+                a = input("Enter a number (integer) or a letter to exit: ")
+                while isinteger(a) == False and a.lower() not in alphabet:
+                    print("Invalid sign.")
+                    a = input("Enter a number (integer) or a letter to exit: ")
+            elif b == "*":
+                total = int(a) * int(c)
+                print("Result: " + str(total))
+                a = input("Enter a number (integer) or a letter to exit: ")
+                while isinteger(a) == False and a.lower() not in alphabet:
+                    print("Invalid sign.")
+                    a = input("Enter a number (integer) or a letter to exit: ")
+            else:
+                total = int(a) / int(c)
+                print("Result: " + str(total))
+                a = input("Enter a number (integer) or a letter to exit: ")
+                while isinteger(a) == False and a.lower() not in alphabet:
+                    print("Invalid sign.")
+                    a = input("Enter a number (integer) or a letter to exit: ")
+
+else:
+    sys.exit("Goodbye!")
